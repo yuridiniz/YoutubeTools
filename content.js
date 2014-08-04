038b4b6817421fc44c5d5cf568ef42d6c775783b
Yuri Araújo <yschmitzz@hotmail.com>
2014-08-04 19:55:31 -0300

    Construção do componente de repetição

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
 
+var verificarAtualizacao = function(){
+	chrome.runtime.sendMessage({
+		callBack: "VerificarAtualizacao",
+		from: "executarClick",
+		title: "Ads removido",
+		body: "Ads do youtube acabou de ser removido"
+	});
+}
 
-remover = function(buscando) {
+var remover = function(buscando) {
     try {
         if (window.location.href.indexOf("youtube.com") > -1) {
             if (document.querySelector(".videoAdUi") != null) {
@@ -41,16 +49,15 @@ remover = function(buscando) {
     }
 }
 
-executarClick = function () {
+var executarClick = function () {
     var video = document.querySelector(".html5-main-video");
 
-    if (video.editado != true) {
+    if (video.volumeEditavel == null) {
         video.volumeEditavel = video.volume;
         video.volume = 0;
-        video.currentTime = 10;
     }
 
-    video.editado = true;
+
 
     if (video.readyState != 4 && document.querySelector(".videoAdUi") != null) {
         document.querySelector(".videoAdUi").style.opacity = "0"
@@ -63,12 +70,10 @@ executarClick = function () {
     }
 
     if (video.editado != true) {
-        video.volumeEditavel = video.volume;
-        video.volume = 0;
         video.currentTime = 10;
     }
 
-
+    video.editado = true;
 
     var ev = document.createEvent("MouseEvents");
     ev.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 2, null);
@@ -84,10 +89,12 @@ executarClick = function () {
             
         }, 100);
     } else {
-        console.log("%cremovido", "color:green")
         video.style.opacity = 1
         video.volume = video.volumeEditavel;
+		console.log("%cremovido", "color:green");
+		
         chrome.runtime.sendMessage({
+			callBack: "AbrirAlerta",
             from: "executarClick",
             title: "Ads removido",
             body: "Ads do youtube acabou de ser removido"
@@ -95,4 +102,138 @@ executarClick = function () {
     }
 }
 
-remover()
+/*
+* Cria controles no HTML para realizar o autoreplay
+*/
+var autoReplay = function () {
+
+    var painelControle = document.querySelector(".html5-player-chrome")
+    var painelModais = document.querySelector(".html5-video-controls");
+
+    var divControle = document.createElement("div");
+    divControle.setAttribute("class", "ytp-button ytp-button-watch-later");
+    divControle.setAttribute("role", "button");
+    divControle.setAttribute("aria-label", "YouTube Tools");
+    divControle.setAttribute("id", "OpenWindowController");
+    painelControle.appendChild(divControle)
+
+
+    var divOpcoes = document.createElement("div");
+    divOpcoes.setAttribute("class", "ytp-menu-html5-stop-propagation classeTeste");
+    divOpcoes.setAttribute("style", "height: 100px;background: rgba(0,0,0,0.5);border: 1px solid #00A0EA;top: 0;display: block;");
+
+    painelModais.insertBefore(divOpcoes, painelModais.childNodes[0]);
+
+
+    var div = document.createElement("div");
+    div.style.position = "fixed";
+    div.style.zIndex = "10000";
+    div.style.bottom = "0px";
+    div.style.right = "0px";
+    div.style.width = "320px"
+    div.style.height = "28px";
+    div.style.padding = "8px"
+    div.style.backgroundColor = "#FFF"
+    div.style.border = "1px solid #CCC"
+    div.id = "controle-tools"
+
+    document.body.appendChild(div)
+
+    var textInicio = document.createElement("input");
+    textInicio.type = "text"
+    textInicio.id = "txtInicio"
+    textInicio.style.width = "100px";
+    textInicio.style.padding = "4px";
+    textInicio.style.margin = "0px 7px";
+    textInicio.setAttribute("placeholder", "Minutos iniciais")
+
+    var txtFim = document.createElement("input");
+    txtFim.type = "text"
+    txtFim.id = "txtFim"
+    txtFim.style.width = "100px";
+    txtFim.style.padding = "4px";
+    txtFim.style.margin = "0px 7px";
+    txtFim.setAttribute("placeholder", "Minutos iniciais")
+
+    var button1 = document.createElement("button");
+    button1.innerText = "Replay"
+    button1.style.padding = "-2px 2px 2px 5px"
+    button1.style.margin = "0px 0px 4px 1px"
+    button1.setAttribute("class","yt-uix-button yt-uix-button-size-default yt-uix-button-default")
+    div.appendChild(textInicio)
+    div.appendChild(txtFim)
+    div.appendChild(button1)
+
+
+    document.querySelector("#OpenWindowController").addEventListener("click", function () {
+
+    }, true);
+
+
+    document.querySelector("#controle-tools button").addEventListener("click", function () {
+
+        try {
+            var inicio = document.querySelector("#controle-tools #txtInicio").value;
+            var fim = document.querySelector("#controle-tools #txtFim").value;
+            
+            if (inicio.indexOf(":") != -1) {
+                valoresInit = inicio.split(":");
+                var minutos = parseInt(valoresInit[0]) * 60;
+                var segundos = parseInt(valoresInit[1]);
+
+                inicio = minutos + segundos
+            }
+
+            if (fim.indexOf(":") != -1) {
+
+                valoresInit = fim.split(":");
+                var minutos = parseInt(valoresInit[0]) * 60;
+                var segundos = parseInt(valoresInit[1]);
+
+                fim = minutos + segundos
+            }
+            if (inicio == "")
+                inicio = 0;
+
+            if (fim == "")
+                fim = document.querySelector(".html5-main-video").duration;
+
+            inicio = parseFloat(inicio)
+            fim = parseFloat(fim);
+
+            if (isNaN(fim) || isNaN(inicio)) {
+                throw new Error("Preencha corretamente os valores, o valor deve conter o formato 02:10 ou em segundos (120.10), caso deixe em branco, o raplay executara o video por completo")
+            }
+
+            if (this.className.indexOf("yt-uix-button-toggled") == -1) {
+                document.querySelector("#controle-tools #txtInicio").disabled = true;
+                document.querySelector("#controle-tools #txtFim").disabled = true;
+                window.intervaloReplay = setInterval(function() {
+                    if (document.querySelector(".ytp-button-replay") != null || document.querySelector(".html5-main-video").currentTime >= fim) {
+                        document.querySelector(".html5-main-video").currentTime = inicio;
+                    }
+                }, 1000)
+
+                this.classList.add("yt-uix-button-toggled")
+
+            } else {
+                clearInterval(window.intervaloReplay)
+                document.querySelector("#controle-tools #txtInicio").disabled = false;
+                document.querySelector("#controle-tools #txtFim").disabled = false;
+                this.classList.remove("yt-uix-button-toggled")
+            }
+        } catch (erro) {
+            chrome.runtime.sendMessage({
+                callBack: "AbrirAlerta",
+                from: "autoReplay",
+                title: "Valores invalidos",
+                body: erro.message
+            });
+        }
+    }, true);
+
+}
+
+autoReplay();
+remover();
+verificarAtualizacao()
--- engine.js
+++ engine.js
@@ -2,39 +2,67 @@
 * Cria um ouvinte de mensagens, recebe as mensagens enviada pelos content_scripts e exibe o rich notification
 *
 */
+
+chrome.notifications.onButtonClicked.addListener(function(notfiId, btnIndex)
+{
+	if(btnIndex == 0)
+		chrome.tabs.create({ url: chrome.app.getDetails().update_url.toString().replace("update.xml","YoutubeTools.zip") });
+});
+
 chrome.runtime.onMessage.addListener(function(msg, sender) {
 
-	
+console.log(msg)
+	if(msg.callBack == "AbrirAlerta")
+		AbrirAlerta(msg);
+		
+	if(msg.callBack == "VerificarAtualizacao")
+		VerificarAtualizacao();
+});
+
+
+
+var VerificarAtualizacao = function()
+{
+	chrome.runtime.requestUpdateCheck(function(status,versao)
+	{
+		if (status == "update_available") {
+		AtualizarYoutubeTools(versao)
+	  } else if (status == "no_update") {
+		console.log("no update found");
+	  } else if (status == "throttled") {
+		console.log("Oops, I'm asking too frequently - I need to back off.");
+	  }
+	});
+};
+
+var AtualizarYoutubeTools = function(versao)
+{
+	AbrirAlerta({
+		from : "engine",
+		title : "Youtube tools",
+		body : "Ha uma versao mais recente disponivel: " + versao.version.toString(),
+		btnList : [ { title : "Iniciar download"}]
+		});
+}
+
+var AbrirAlerta = function(msg) {
+
     if(msg.from == undefined) return;
     if(msg.body == undefined) return;
     if(msg.title == undefined) return;
+	if(msg.btnList == undefined) msg.btnList = [];
 
-    chrome.notifications.create('0',{
+    chrome.notifications.create(msg.from,{
 		  type:'basic',
 		  title:msg.title,
 		  iconUrl: 'http://www.rfimasters.com/wp-content/uploads/2013/09/warning_notification-1331px-300x289.png',
 		  message: msg.body,
 		  expandedMessage:'Não esqueça de efetuar uma colaboração :p',
 		  priority:1,
-		  isClickable:true
+		  isClickable:true,
+		  buttons : msg.btnList
 		},function(id){
-			console.error(chrome.runtime.lastError)
+			console.log("callback")
 		});
-});
+}
 
-chrome.runtime.onUpdateAvailable
-
-chrome.runtime.onUpdateAvailable.addListener(function(e,v)
-{
-console.log(e)
-console.log(v)
-});
-
-var x = chrome.runtime.requestUpdateCheck(function(e)
-{
-console.log(e)
-});
-
-console.log(x)
-
-console.log(chrome.runtime)
